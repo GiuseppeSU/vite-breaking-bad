b84d989d62707b0534df4431de48a76144473de6
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-03-29 15:30:54 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import MyHeader from './components/MyHeader.vue';
+export default {
+  components: {
+    MyHeader
+  }
+}
+
+</script>
+
+<template>
+  <header>
+    <MyHeader></MyHeader>
+  </header>
+
+  <main>
+
+  </main>
+
+  <footer></footer>
+</template>
+
+<style scoped lang="scss">
+@use './styles/general.scss';
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'MyHeader'
+
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img src="../assets/yu-gi-oh-logo.jpg" alt="Yu-Gi-Ho logo" class=" my_image ">
+                <h1>Yu-gi-oh Api</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped lang="scss">
+.my_image {
+    height: 60px;
+}
+</style>
